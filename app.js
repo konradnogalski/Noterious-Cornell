e3bbb4d158c66db61e55381616eb194db7f0031e
Konrad Nogalski <konrad.nogalski+github@gmail.com>
2020-02-02 11:45:52 +0100

    Server backbone

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express(); //konwencja - reprezentuje moduł Express
+
+app.get("/", function(req, res)
+{
+  res.send("Szkielet aplikacji do tworzenia notatek systemem Cornella.")
+});
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
+});
