8f5a6a27cdcd3b242b1a7f8542b7484dc3aead9f
Konrad Nogalski <konrad.nogalski+github@gmail.com>
2020-02-26 23:58:47 +0100

    Major refactor
    1) row highlight and show action buttons using CSS instead of JS
    2) Simplified code for show more / show less feature. Based more on css 
    then js events.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,6 @@
+const maxKeywordsShow = $("tbody").attr("max-keywords");
+const maxNotesShow = $("tbody").attr("max-paragraphs");
+
 attachEventHandlers();
 
 function attachEventHandlers() {
@@ -9,26 +12,9 @@ function attachEventHandlers() {
 function attachNoteRowListeners() {
   const noteRows = $("tbody > tr");
 
-  noteRows.on("mouseenter mouseleave", function(event) {
-
-    var wasMouseEnterEventRelatedTargetRemovedFromDOM = $(document).find(event.relatedTarget).length === 0;
-    if (event.type === "mouseenter" && wasMouseEnterEventRelatedTargetRemovedFromDOM){
-      //do not trigger event
-      return;
-    }
-
-    toggleHighlight(this);
-    toggleActionButtonsVisiblity(this);
-  });
-
-  noteRows.find("p.show-more").click(function() {
+  noteRows.click(function() {
     const parentRowElement = $(this).parent().parent();
-    showMore(parentRowElement);
-  });
-
-  noteRows.find("p.show-less").click(function() {
-    const parentRowElement = $(this).parent().parent();
-    showLess(parentRowElement);
+    showMore(this);
   });
 }
 
@@ -45,101 +31,59 @@ function attachClearButtonOnClickEventHandler() {
   })
 }
 
-function toggleHighlight(noteRow) {
-  $(noteRow).toggleClass("table-info");
-}
-
-function toggleActionButtonsVisiblity(noteRow) {
-  $(noteRow).find(".btn").toggleClass("invisible");
-}
-
 function showMore(tableRow) {
-  // if (!shouldShowMore(tableRow)) {
-  //   return;
-  // }
+  const keywords = getNoteKeywords(tableRow);
+  const notes = getNoteNotes(tableRow);
 
-  getNoteUsingAjaxCall(tableRow, function(response) {
-    const tbody = $(tableRow).parent();
-    const maxKeywordCount = tbody.attr("max-keywords");
-    const maxParagraphCount = tbody.attr("max-paragraphs");
+  if (keywords.length > maxKeywordsShow){
+    keywords.removeClass("d-none");
+    keywords.last().text("Click to show less...");
+  }
 
-    var keywordsCell = $(tableRow).find("td:nth-child(2)");
-    var notesCell = $(tableRow).find("td:nth-child(3)")
+  if (notes.length > maxNotesShow){
+    notes.removeClass("d-none");
+    notes.last().text("Click to show less...");
+  }
 
-    if (keywordsCell[0].childElementCount > maxKeywordCount){
-      keywordsCell.empty();
-      response.keywords.split(/\r\n/).forEach(element => {
-        keywordsCell
-          .append("<p class='mb-1 text-uppercase text-monospace'>" + element + "</p>");
-        });
-      keywordsCell.append("<p class='font-italic show-less'>Click to show less...</p>");
-    }
-
-    if (notesCell[0].childElementCount > maxParagraphCount){
-
-      notesCell.empty()
-      response.notes.split(/\r\n/).forEach(element => {
-        notesCell
-          .append("<p class='mb-1'>" + element + "</p");
-        });
-      notesCell.append("<p class='font-italic show-less'>Click to show less...</p>");
-    }
-
-    $(tableRow).find("p.show-less").click(function() {
-      const parentRowElement = $(this).parent().parent();
-      showLess(parentRowElement);
-    });
+  $(tableRow).click(function() {
+    showLess(this);
   });
 }
 
 function showLess(tableRow) {
-  const tbody = $(tableRow).parent();
-  const maxKeywordCount = tbody.attr("max-keywords");
-  const maxParagraphCount = tbody.attr("max-paragraphs");
+  const keywords = getNoteKeywords(tableRow);
+  const notes = getNoteNotes(tableRow);
 
-  const keywordsCell = $(tableRow).find("td:nth-child(2)");
-  const notesCell = $(tableRow).find("td:nth-child(3)")
-
-  if (keywordsCell[0].childElementCount > maxKeywordCount){
-    for(let i=keywordsCell[0].childElementCount - 1; keywordsCell[0].childElementCount > maxKeywordCount; i++){
-      keywordsCell[0].childNodes[keywordsCell[0].childElementCount - 1].remove();
-    }
-    keywordsCell.append("<p class='font-italic show-more'>Click to show more...</p>");
+  if (keywords.length > maxKeywordsShow){
+    keywords.filter(index => index > maxKeywordsShow - 1 && index !== keywords.length - 1).addClass("d-none");
+    keywords.last().text("Click to show more...");
   }
 
-  if (notesCell[0].childElementCount > maxParagraphCount){
-    for(let i=notesCell[0].childElementCount - 1; notesCell[0].childElementCount > maxKeywordCount; i++){
-      notesCell[0].childNodes[notesCell[0].childElementCount - 1].remove();
-    }
-    notesCell.append('<p class="font-italic show-more">Click to show more...</p>');
-  };
+  if (notes.length > maxNotesShow){
+    notes.filter(index => index > maxNotesShow - 1 && index !== notes.length - 1).addClass("d-none");
+    notes.last().text("Click to show more...");
+  }
 
-  tableRow.find("p.show-more").click(function() {
-    const parentRowElement = $(this).parent().parent();
-    showMore(parentRowElement);
+  $(tableRow).click(function() {
+    showMore(this);
   });
+}
+
+function getNoteKeywords(tableRow){
+  return getRowCell(tableRow, 2);
+}
+
+function getNoteNotes(tableRow){
+  return getRowCell(tableRow, 3);
+}
+
+function getRowCell(tableRow, columnPosition){
+  return $("tr[noteid='" + getNoteId(tableRow) + "'] td:nth-child(" + columnPosition +") p");
 }
 
 function editNote(noteRow) {
   const noteId = getNoteId(noteRow);
   window.location.href = "http://localhost:3000/editnote/" + noteId;
-}
-
-function getNoteUsingAjaxCall(noteRow, onSuccess) {
-  $.ajax({
-    url: "http://localhost:3000/notes/" + getNoteId(noteRow),
-    success: function(response) {
-      onSuccess(response);
-    }
-  });
-}
-
-function isHeaderRow(tableRow) {
-  return typeof $(tableRow).attr("noteid") === "undefined";
-}
-
-function shouldShowMore(noteRow) {
-  return $(noteRow).attr("showMore") === "true";
 }
 
 function getNoteId(noteRow) {
